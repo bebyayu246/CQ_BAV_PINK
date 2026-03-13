ca06f69c4b9e7870c26004c8a8cbe8eea67ee665
bebyayu246 <beby.ayu.ft24@mail.umy.ac.id>
2026-03-13 13:54:23 +0700

    meng isi kode hitung jumlah mata kuliah

--- Form1.cs
+++ Form1.cs
@@ -70,6 +70,30 @@ namespace PraktikumADO
             }
         }
 
+        // Praktikum 3 - Hitung Jumlah Mata Kuliah
+        private void btnHitungMK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Koneksi();
+                conn.Open();
+
+                string query = "SELECT COUNT(*) FROM MataKuliah";
+
+                cmd = new SqlCommand(query, conn);
+
+                int jumlah = (int)cmd.ExecuteScalar();
+
+                txtHasil.Text = jumlah.ToString();
+
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
